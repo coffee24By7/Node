afe094e7f8f6e0b0a9287efbcb7c0a169bd11462
Eugene Larkin <eugenelarkin66@outlook.com>
2020-04-20 01:24:33 -0400

    video 12

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+require('../models/Store');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
@@ -7,6 +11,8 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 };
